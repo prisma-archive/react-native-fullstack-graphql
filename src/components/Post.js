728409284604c59ce189f377cdc7f58e94e75f11
Tim Suchanek <tim.suchanek@gmail.com>
2016-12-08 18:01:39 +0100

    Update width & height of Image

--- src/components/Post.js
+++ src/components/Post.js
@@ -10,10 +10,10 @@ export default class Post extends React.Component {
 
   render () {
     return (
-      <View style={{flex: 1, width: 200, height: 200}}>
+      <View style={{flex: 1, width: 150, height: 150}}>
         <Image
           source={{ uri: this.props.post.imageUrl }}
-          style={{flex: 1}}
+          style={{flex: 1, width: 150, height: 150}}
         />
         <Text>{this.props.post.description}</Text>
       </View>
