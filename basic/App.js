38049da7f40264d14ce6f2eee97a6b99d16e0bf2
Devan Beitel <devan@devanb.us>
2018-01-04 20:57:04 -0500

    Corrects a few dependency versions and removes dead code.

--- basic/App.js
+++ basic/App.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { ApolloProvider } from 'react-apollo'
-import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset'
-import ListPage from './components/ListPage'
+import React from 'react';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset';
+import ListPage from './components/ListPage';
 
-const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
 
 const client = new ApolloClient({
   link: httpLink,
-  cache: new InMemoryCache(),
-})
+  cache: new InMemoryCache()
+});
 
 export default class App extends React.Component {
   render() {
@@ -17,15 +16,6 @@ export default class App extends React.Component {
       <ApolloProvider client={client}>
         <ListPage />
       </ApolloProvider>
-    )
+    );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
